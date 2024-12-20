b9cfd90a54c7bda3a5849f64c75e42f9cdcce48b
syjn99 <syjn99@gmail.com>
2024-12-20 21:41:34 +0900

    feat: add tracing & match verbosity with log level

--- bin/ream/src/main.rs
+++ bin/ream/src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use tracing::{info, Level};
+use tracing_subscriber;
 
 use ream::cli::{Cli, Commands};
 
@@ -7,7 +9,16 @@ fn main() {
 
     match cli.command {
         Commands::Node(cmd) => {
-            println!("Starting node with verbosity {}", cmd.verbosity);
+            let level = match cmd.verbosity {
+                0 => Level::ERROR,
+                1 => Level::WARN,
+                2 => Level::INFO,
+                3 => Level::DEBUG,
+                _ => Level::TRACE,
+            };
+            tracing_subscriber::fmt().with_max_level(level).init();
+
+            info!("Starting node with verbosity level {:?}", level);
         }
     }
 }
