5d69586f6878943a7d6c05b37a9bc8909576580a
syjn99 <syjn99@gmail.com>
2025-01-30 23:12:40 +0900

    feat(testing): add snappy decoder

---
+++ testing/ef-tests/src/utils.rs
@@ -0,0 +1,9 @@
+use snap::raw::Decoder;
+use std::path::Path;
+
+pub fn read_ssz_snappy<T: ssz::Decode>(path: &Path) -> Option<T> {
+    let ssz_snappy = std::fs::read(path).ok()?;
+    let mut decoder = Decoder::new();
+    let ssz = decoder.decompress_vec(&ssz_snappy).unwrap();
+    T::from_ssz_bytes(&ssz).ok()
+}
