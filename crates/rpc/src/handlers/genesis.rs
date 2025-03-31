c53a5f6b424d8f6a91a6e2df7f29b4bda8a77c14
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2025-03-31 01:39:25 -0600

    fix(rpc): forgot "data": T for genesis rpc (#259)

--- crates/rpc/src/handlers/genesis.rs
+++ crates/rpc/src/handlers/genesis.rs
@@ -1,12 +1,13 @@
 use ream_consensus::genesis::Genesis;
-use serde_json::json;
 use warp::{
     http::status::StatusCode,
     reject::Rejection,
-    reply::{Reply, json, with_status},
+    reply::{Reply, with_status},
 };
+
+use super::Data;
 
 /// Called by `/genesis` to get the Genesis Config of Beacon Chain.
 pub async fn get_genesis(genesis: Genesis) -> Result<impl Reply, Rejection> {
-    Ok(with_status(json(&json!(genesis)), StatusCode::OK))
+    Ok(with_status(Data::json(genesis), StatusCode::OK))
 }
--- crates/rpc/src/handlers/mod.rs
+++ crates/rpc/src/handlers/mod.rs
@@ -1,1 +1,23 @@
 pub mod genesis;
+
+use serde::Serialize;
+use serde_json::json;
+use warp::reply::{Json, json};
+
+/// A generic data struct that can be used to wrap any data type
+/// used for json rpc responses
+///
+/// # Example
+/// {
+///  "data": json!(T)
+/// }
+#[derive(Debug, Serialize)]
+pub struct Data<T> {
+    pub data: T,
+}
+
+impl<T: Serialize> Data<T> {
+    pub fn json(data: T) -> Json {
+        json(&json!(Self { data }))
+    }
+}
