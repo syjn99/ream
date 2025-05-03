ffd6ca62e123bc74761df0c5a18a99f43d9a86ae
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2025-05-03 00:48:47 -0600

    chore: bump libraries to latest major/minor version (#363)

--- crates/networking/p2p/src/network.rs
+++ crates/networking/p2p/src/network.rs
@@ -10,21 +10,29 @@ use anyhow::anyhow;
 use discv5::Enr;
 use libp2p::{
     Multiaddr, PeerId, Swarm, SwarmBuilder, Transport, connection_limits,
-    core::{muxing::StreamMuxerBox, transport::Boxed},
+    core::{
+        muxing::StreamMuxerBox,
+        transport::Boxed,
+        upgrade::{SelectUpgrade, Version},
+    },
+    dns::Transport as DnsTransport,
     futures::StreamExt,
     identify,
     multiaddr::Protocol,
-    noise,
+    noise::Config as NoiseConfig,
     swarm::{NetworkBehaviour, SwarmEvent},
+    tcp::{Config as TcpConfig, tokio::Transport as TcpTransport},
     yamux,
 };
 use libp2p_identity::{Keypair, PublicKey, secp256k1};
+use libp2p_mplex::{MaxBufferBehaviour, MplexConfig};
 use ream_discv5::{
     config::NetworkConfig,
     discovery::{DiscoveredPeers, Discovery},
 };
 use ream_executor::ReamExecutor;
 use tracing::{error, info, warn};
+use yamux::Config as YamuxConfig;
 
 #[derive(NetworkBehaviour)]
 pub(crate) struct ReamBehaviour {
@@ -237,23 +245,20 @@ impl Network {
 type BoxedTransport = Boxed<(PeerId, StreamMuxerBox)>;
 pub fn build_transport(local_private_key: Keypair) -> std::io::Result<BoxedTransport> {
     // mplex config
-    let mut mplex_config = libp2p_mplex::MplexConfig::new();
+    let mut mplex_config = MplexConfig::new();
     mplex_config.set_max_buffer_size(256);
-    mplex_config.set_max_buffer_behaviour(libp2p_mplex::MaxBufferBehaviour::Block);
+    mplex_config.set_max_buffer_behaviour(MaxBufferBehaviour::Block);
 
-    let yamux_config = yamux::Config::default();
+    let yamux_config = YamuxConfig::default();
 
-    let tcp = libp2p::tcp::tokio::Transport::new(libp2p::tcp::Config::default().nodelay(true))
-        .upgrade(libp2p::core::upgrade::Version::V1)
-        .authenticate(noise::Config::new(&local_private_key).expect("Noise disabled"))
-        .multiplex(libp2p::core::upgrade::SelectUpgrade::new(
-            yamux_config,
-            mplex_config,
-        ))
+    let tcp = TcpTransport::new(TcpConfig::default().nodelay(true))
+        .upgrade(Version::V1)
+        .authenticate(NoiseConfig::new(&local_private_key).expect("Noise disabled"))
+        .multiplex(SelectUpgrade::new(yamux_config, mplex_config))
         .timeout(Duration::from_secs(10));
     let transport = tcp.boxed();
 
-    let transport = libp2p::dns::tokio::Transport::system(transport)?.boxed();
+    let transport = DnsTransport::system(transport)?.boxed();
 
     Ok(transport)
 }
