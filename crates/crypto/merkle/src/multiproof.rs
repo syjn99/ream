6590f02c5db651c72f87f3848a95948d1157af85
Jun Song <syjn99@gmail.com>
2025-05-15 17:13:48 +0900

    fix: apply review from Kolby - rename variables, etc.

--- crates/crypto/merkle/src/multiproof.rs
+++ crates/crypto/merkle/src/multiproof.rs
@@ -3,7 +3,7 @@
 use std::collections::{BTreeMap, HashMap};
 
 use alloy_primitives::B256;
-use anyhow::ensure;
+use anyhow::{anyhow, ensure};
 use serde::{Deserialize, Serialize};
 
 use crate::{
@@ -34,10 +34,10 @@ impl Multiproof {
         let bottom_length: u64 = 1 << DEPTH;
 
         ensure!(!indices.is_empty(), "Indices cannot be empty");
-        for &index in indices {
-            ensure!(index < bottom_length, "Index out of bounds");
-        }
-
+        ensure!(
+            indices.iter().all(|&index| index < bottom_length),
+            "Index out of bounds"
+        );
         let generalized_indices = indices
             .iter()
             .map(|&index| generalized_index_from_leaf_index(index, DEPTH))
@@ -46,20 +46,17 @@ impl Multiproof {
 
         let leaves = generalized_indices
             .iter()
-            .map(|&g| (g, tree[g as usize]))
+            .map(|&generalized_index| (generalized_index, tree[generalized_index as usize]))
             .collect::<HashMap<u64, B256>>();
         let proofs = helper_indices
             .iter()
-            .map(|&g| (g, tree[g as usize]))
+            .map(|&generalized_index| (generalized_index, tree[generalized_index as usize]))
             .collect::<BTreeMap<u64, B256>>();
 
         Ok(Self { leaves, proofs })
     }
 
     /// Return the root of the multiproof.
-    ///
-    /// Note: This function is a Rust port of the original function
-    /// (``calculate_multi_merkle_root()``) at spec code: https://github.com/ethereum/consensus-specs/blob/c27589872ac2dbafb566dcf7896c3fa975f00fe9/ssz/merkle-proofs.md?plain=1#L318-L341
     pub fn calculate_root(&self) -> anyhow::Result<B256> {
         let leaf_indices = self.leaves.keys().cloned().collect::<Vec<_>>();
         let helper_indices = get_helper_indices(&leaf_indices);
@@ -78,13 +75,14 @@ impl Multiproof {
         }
 
         let mut keys = objects.keys().cloned().collect::<Vec<_>>();
-        keys.sort_by(|a, b| b.cmp(a)); // Sort in descending order
+        // Sort in descending order
+        keys.sort_by(|a, b| b.cmp(a));
 
-        let mut pos = 0;
-        while pos < keys.len() {
+        let mut position = 0;
+        while position < keys.len() {
             let key = keys
-                .get(pos)
-                .ok_or_else(|| anyhow::anyhow!("Missing key at position {}", pos))?;
+                .get(position)
+                .ok_or_else(|| anyhow!("Missing key at position {position}"))?;
             let parent_index = generalized_index_parent(*key);
 
             let key_present = objects.contains_key(key);
@@ -96,30 +94,30 @@ impl Multiproof {
                 let left_index = generalized_index_sibling(right_index);
                 let left_input = objects
                     .get(&left_index)
-                    .ok_or_else(|| anyhow::anyhow!("Missing left node at index {}", left_index))?;
-                let right_input = objects.get(&right_index).ok_or_else(|| {
-                    anyhow::anyhow!("Missing right node at index {}", right_index)
-                })?;
+                    .ok_or_else(|| anyhow!("Missing left node at index {left_index}"))?;
+                let right_input = objects
+                    .get(&right_index)
+                    .ok_or_else(|| anyhow!("Missing right node at index {right_index}"))?;
 
                 *objects.entry(parent_index).or_default() =
                     hash_concat(left_input.as_slice(), right_input.as_slice());
                 keys.push(parent_index);
             }
-            pos += 1;
+            position += 1;
         }
 
         let root = *objects
             .get(&1)
-            .ok_or_else(|| anyhow::anyhow!("Missing root node at index 1"))?;
+            .ok_or_else(|| anyhow!("Missing root node at index 1"))?;
         Ok(root)
     }
 
     /// Verify the multiproof against the given root.
-    pub fn verify(&self, root: B256) -> anyhow::Result<()> {
-        let calculated = self.calculate_root()?;
+    pub fn verify(&self, expected_root: B256) -> anyhow::Result<()> {
+        let calculated_root = self.calculate_root()?;
         ensure!(
-            calculated == root,
-            "Invalid proof: expected root {root:?}, but got {calculated:?}"
+            calculated_root == expected_root,
+            "Invalid proof: expected root {expected_root:?}, but got {calculated_root:?}"
         );
         Ok(())
     }
