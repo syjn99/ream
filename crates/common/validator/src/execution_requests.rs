8dc416bf1c41160a9aa14c0b0aaff87e82492ea1
Patchoulis <76623289+Patchoulis@users.noreply.github.com>
2025-05-21 14:21:42 -0600

    feat: implement prepare_execution_payload (#420)
    * feat: added prepare_execution_payload
    
    Update Cargo.toml
    
    * fix: applied suggestions

--- crates/common/validator/src/execution_requests.rs
+++ crates/common/validator/src/execution_requests.rs
@@ -1,19 +1,29 @@
-use alloy_primitives::Bytes;
-use anyhow::{Result, anyhow, bail, ensure};
+use alloy_primitives::{Address, B256, Bytes};
+use anyhow::{anyhow, bail, ensure};
 use ream_consensus::{
     consolidation_request::ConsolidationRequest,
     constants::{CONSOLIDATION_REQUEST_TYPE, DEPOSIT_REQUEST_TYPE, WITHDRAWAL_REQUEST_TYPE},
     deposit_request::DepositRequest,
+    electra::beacon_state::BeaconState,
     execution_requests::ExecutionRequests,
     withdrawal_request::WithdrawalRequest,
+};
+use ream_execution_engine::{
+    ExecutionEngine,
+    rpc_types::forkchoice_update::{
+        ForkchoiceStateV1, ForkchoiceUpdateResult, PayloadAttributesV3,
+    },
 };
 use ssz::Decode;
 use ssz_types::{
     VariableList,
     typenum::{U2, U16, U8192},
 };
+use tree_hash::TreeHash;
 
-pub fn get_execution_requests(execution_requests_list: Vec<Bytes>) -> Result<ExecutionRequests> {
+pub fn get_execution_requests(
+    execution_requests_list: Vec<Bytes>,
+) -> anyhow::Result<ExecutionRequests> {
     let mut deposits = None;
     let mut withdrawals = None;
     let mut consolidations = None;
@@ -71,4 +81,30 @@ pub fn get_execution_requests(execution_requests_list: Vec<Bytes>) -> Result<Exe
         withdrawals: withdrawals.unwrap_or_default(),
         consolidations: consolidations.unwrap_or_default(),
     })
+}
+
+pub async fn prepare_execution_payload(
+    state: BeaconState,
+    safe_block_hash: B256,
+    finalized_block_hash: B256,
+    suggested_fee_recipient: Address,
+    execution_engine: ExecutionEngine,
+) -> anyhow::Result<ForkchoiceUpdateResult> {
+    let (withdrawals, _) = state.get_expected_withdrawals()?;
+    execution_engine
+        .engine_forkchoice_updated_v3(
+            ForkchoiceStateV1 {
+                head_block_hash: state.latest_execution_payload_header.block_hash,
+                safe_block_hash,
+                finalized_block_hash,
+            },
+            Some(PayloadAttributesV3 {
+                timestamp: state.compute_timestamp_at_slot(state.slot),
+                prev_randao: state.get_randao_mix(state.get_current_epoch()),
+                suggested_fee_recipient,
+                withdrawals: withdrawals.into(),
+                parent_beacon_block_root: state.latest_block_header.tree_hash_root(),
+            }),
+        )
+        .await
 }
