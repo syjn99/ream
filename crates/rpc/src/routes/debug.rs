0b60295ad35c0626aefb0f09e2841a8059332290
Jun Song <syjn99@gmail.com>
2025-06-25 07:25:54 +0900

    fix: register /beacon/heads to /eth/v2/debug

--- crates/rpc/src/routes/debug.rs
+++ crates/rpc/src/routes/debug.rs
@@ -2,10 +2,10 @@ use actix_web::web::{ServiceConfig, scope};
 
 use crate::handlers::{block::get_beacon_heads, state::get_beacon_state};
 
-pub fn register_debug_routes(cfg: &mut ServiceConfig) {
-    cfg.service(scope("/debug").service(get_beacon_heads));
-}
-
 pub fn register_debug_routes_v2(cfg: &mut ServiceConfig) {
-    cfg.service(scope("/debug").service(get_beacon_state));
+    cfg.service(
+        scope("/debug")
+            .service(get_beacon_state)
+            .service(get_beacon_heads),
+    );
 }
