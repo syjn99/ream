57c3113ae005c97555ad03a79c963ba5c524df70
Jun Song <87601811+syjn99@users.noreply.github.com>
2025-08-25 21:13:18 +0900

    fix(account_manager): panicking bug for wrong lifetime setup & log the key_gen time (#746)

--- crates/common/account_manager/src/lib.rs
+++ crates/common/account_manager/src/lib.rs
@@ -9,19 +9,15 @@ pub fn generate_keys(
     activation_epoch: usize,
     num_active_epochs: usize,
 ) -> (PublicKey, PrivateKey) {
-    info!("Generating lean consensus validator keys.....");
+    info!(
+        "Generating lean consensus validator keys  with activation_epoch={activation_epoch}, num_active_epochs={num_active_epochs}....."
+    );
 
     // Hash the seed phrase to get a 32-byte seed
     let mut hasher = Sha256::new();
     hasher.update(seed_phrase.as_bytes());
     let seed = hasher.finalize().into();
     info!("Seed: {seed:?}");
-
-    info!(
-        "Generating hash-based signature key pair with activation_epoch={activation_epoch}, num_active_epochs={num_active_epochs}"
-    );
-
-    info!("Key generation complete");
 
     PrivateKey::generate(
         &mut <ChaCha20Rng as SeedableRng>::from_seed(seed),
