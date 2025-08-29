23e93fba74f359f9b292652784976df45372cabb
Jay <47923507+jveer634@users.noreply.github.com>
2025-08-30 02:59:56 +0530

    feat: implementation of publishing gossipsub messages over P2P (#725)
    * add: gossipsub publish voluntary_exit
    
    * add: gossip publish signed bls
    
    * add: gossip publish signed bls
    
    * fix: codin styles

--- crates/rpc/beacon/src/handlers/pool.rs
+++ crates/rpc/beacon/src/handlers/pool.rs
@@ -29,8 +29,9 @@ use crate::handlers::state::get_state_from_id;
 pub async fn get_bls_to_execution_changes(
     operation_pool: Data<Arc<OperationPool>>,
 ) -> Result<impl Responder, ApiError> {
-    let signed_bls_to_execution_changes = operation_pool.get_signed_bls_to_execution_changes();
-    Ok(HttpResponse::Ok().json(DataResponse::new(signed_bls_to_execution_changes)))
+    Ok(HttpResponse::Ok().json(DataResponse::new(
+        operation_pool.get_signed_bls_to_execution_changes(),
+    )))
 }
 
 /// POST /eth/v1/beacon/pool/bls_to_execution_changes
@@ -38,6 +39,7 @@ pub async fn get_bls_to_execution_changes(
 pub async fn post_bls_to_execution_changes(
     db: Data<ReamDB>,
     operation_pool: Data<Arc<OperationPool>>,
+    network_manager: Data<NetworkManagerService>,
     signed_bls_to_execution_change: Json<SignedBLSToExecutionChange>,
 ) -> Result<impl Responder, ApiError> {
     let highest_slot = db
@@ -61,9 +63,17 @@ pub async fn post_bls_to_execution_changes(
         ))
     })?;
 
+    network_manager
+        .as_ref()
+        .p2p_sender
+        .send_gossip(GossipMessage {
+            topic: GossipTopic {
+                fork: beacon_state.fork.current_version,
+                kind: GossipTopicKind::BlsToExecutionChange,
+            },
+            data: signed_bls_to_execution_change.as_ssz_bytes(),
+        });
     operation_pool.insert_signed_bls_to_execution_change(signed_bls_to_execution_change);
-    // TODO: publish bls_to_execution_change to peers (gossipsub) - https://github.com/ReamLabs/ream/issues/556
-
     Ok(HttpResponse::Ok())
 }
 
@@ -72,8 +82,9 @@ pub async fn post_bls_to_execution_changes(
 pub async fn get_voluntary_exits(
     operation_pool: Data<Arc<OperationPool>>,
 ) -> Result<impl Responder, ApiError> {
-    let signed_voluntary_exits = operation_pool.get_signed_voluntary_exits();
-    Ok(HttpResponse::Ok().json(DataResponse::new(signed_voluntary_exits)))
+    Ok(HttpResponse::Ok().json(DataResponse::new(
+        operation_pool.get_signed_voluntary_exits(),
+    )))
 }
 
 /// POST /eth/v1/beacon/pool/voluntary_exits
@@ -81,6 +92,7 @@ pub async fn get_voluntary_exits(
 pub async fn post_voluntary_exits(
     db: Data<ReamDB>,
     operation_pool: Data<Arc<OperationPool>>,
+    network_manager: Data<NetworkManagerService>,
     signed_voluntary_exit: Json<SignedVoluntaryExit>,
 ) -> Result<impl Responder, ApiError> {
     let highest_slot = db
@@ -104,9 +116,18 @@ pub async fn post_voluntary_exits(
             ))
         })?;
 
-    operation_pool.insert_signed_voluntary_exit(signed_voluntary_exit);
-    // TODO: publish voluntary exit to peers (gossipsub) - https://github.com/ReamLabs/ream/issues/556
+    network_manager
+        .as_ref()
+        .p2p_sender
+        .send_gossip(GossipMessage {
+            topic: GossipTopic {
+                fork: beacon_state.fork.current_version,
+                kind: GossipTopicKind::VoluntaryExit,
+            },
+            data: signed_voluntary_exit.as_ssz_bytes(),
+        });
 
+    operation_pool.insert_signed_voluntary_exit(signed_voluntary_exit);
     Ok(HttpResponse::Ok())
 }
 
