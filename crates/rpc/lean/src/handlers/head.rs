7f6661dc7d3e931685d85b9cb2a59221a334c9d8
Jun Song <87601811+syjn99@users.noreply.github.com>
2025-09-02 07:51:04 +0900

    fix: use `LeanChainReader` for lean http server param (#759)
    fix: use LeanChainReader for lean http server param

--- crates/rpc/lean/src/handlers/head.rs
+++ crates/rpc/lean/src/handlers/head.rs
@@ -1,16 +1,11 @@
-use std::sync::Arc;
-
 use actix_web::{HttpResponse, Responder, get, web::Data};
 use ream_api_types_beacon::error::ApiError;
 use ream_api_types_lean::head::Head;
-use ream_chain_lean::lean_chain::LeanChain;
-use tokio::sync::RwLock;
+use ream_chain_lean::lean_chain::LeanChainReader;
 
 // GET /lean/v0/head
 #[get("/head")]
-pub async fn get_head(
-    lean_chain: Data<Arc<RwLock<LeanChain>>>,
-) -> Result<impl Responder, ApiError> {
+pub async fn get_head(lean_chain: Data<LeanChainReader>) -> Result<impl Responder, ApiError> {
     Ok(HttpResponse::Ok().json(Head {
         head: lean_chain.read().await.head,
     }))
