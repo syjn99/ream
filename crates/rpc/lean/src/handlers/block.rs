2664e85d9988ad591357ac2e38898edd1129795e
Jun Song <syjn99@gmail.com>
2025-09-03 15:15:55 +0900

    chore: make id::ID common for both beacon and lean

--- crates/rpc/lean/src/handlers/block.rs
+++ crates/rpc/lean/src/handlers/block.rs
@@ -3,13 +3,13 @@ use actix_web::{
     web::{Data, Path},
 };
 use ream_api_types_beacon::error::ApiError;
-use ream_api_types_lean::block_id::BlockID;
+use ream_api_types_common::id::ID;
 use ream_chain_lean::lean_chain::LeanChainReader;
 
 // GET /lean/v0/blocks/{block_id}
 #[get("/blocks/{block_id}")]
 pub async fn get_block(
-    block_id: Path<BlockID>,
+    block_id: Path<ID>,
     lean_chain: Data<LeanChainReader>,
 ) -> Result<impl Responder, ApiError> {
     // Obtain read guard first from the reader.
@@ -17,20 +17,20 @@ pub async fn get_block(
 
     Ok(HttpResponse::Ok().json(
         match block_id.into_inner() {
-            BlockID::Finalized => {
+            ID::Finalized => {
                 lean_chain.get_block_by_root(lean_chain.latest_finalized_hash().ok_or(
                     ApiError::InternalError("Failed to get latest finalized hash".to_string()),
                 )?)
             }
-            BlockID::Genesis => lean_chain.get_block_by_root(lean_chain.genesis_hash),
-            BlockID::Head => lean_chain.get_block_by_root(lean_chain.head),
-            BlockID::Justified => {
+            ID::Genesis => lean_chain.get_block_by_root(lean_chain.genesis_hash),
+            ID::Head => lean_chain.get_block_by_root(lean_chain.head),
+            ID::Justified => {
                 lean_chain.get_block_by_root(lean_chain.latest_justified_hash().ok_or(
                     ApiError::InternalError("Failed to get latest justified hash".to_string()),
                 )?)
             }
-            BlockID::Slot(slot) => lean_chain.get_block_by_slot(slot),
-            BlockID::Root(root) => lean_chain.get_block_by_root(root),
+            ID::Slot(slot) => lean_chain.get_block_by_slot(slot),
+            ID::Root(root) => lean_chain.get_block_by_root(root),
         }
         .ok_or_else(|| ApiError::NotFound("Block not found".to_string()))?,
     ))
